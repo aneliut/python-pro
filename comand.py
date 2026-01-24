6b7200cc52ef1cf6f676be3818d3cc503c1c9546
aneliut <juandavidperdomoromero0@gmail.com>
2026-01-24 12:20:55 -0500

    script de discord
    Updated command prefix to '.' and added duck command.

--- comand.py
+++ comand.py
@@ -1,10 +1,25 @@
 import discord
+
+import requests
+
+import random
+
 from discord.ext import commands
+
+import os
+
+
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='$', intents=intents)
+bot = commands.Bot(command_prefix='.', intents=intents)
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
 
 @bot.event
 async def on_ready():
@@ -16,3 +31,23 @@ async def hello(ctx):
 @bot.command()
 async def heh(ctx):
     await ctx.send("hehehe")
+
+@bot.command()
+async def mem(ctx):
+    image = random.choices(os.listdir('images')) [0]
+
+    with open(f'images/{image}', 'rb') as f:
+        # ¡Vamos a almacenar el archivo de la biblioteca Discord convertido en esta variable!
+        picture = discord.File(f)
+    # A continuación, podemos enviar este archivo como parámetro.
+    await ctx.send(file=picture)
+
+@bot.command('duck')
+async def duck(ctx):
+    '''Una vez que llamamos al comando duck, 
+    el programa llama a la función get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+bot.run('pon tu token aqui :D')
+
